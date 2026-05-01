3d1a16f5997e3f8a4bef94cffb8089c493becb98
treeform <starplant@gmail.com>
2026-05-01 14:55:34 -0700

    merge mac into the test

--- tests/test.nim
+++ tests/test.nim
@@ -9,5 +9,19 @@ when defined(amd64):
   doAssert cast[array[4, float32]](c) == [2.0.float32, 2.0, 2.0, 2.0]
 
   echo checkInstructionSets({SSE3})
-else:
-  echo "Skipping x86 runtime check test"
+elif defined(macosx) and defined(arm64):
+  import nimsimd/neon
+
+  var
+    input: array[16, uint8]
+    output: array[16, uint8]
+
+  for i in 0 ..< input.len:
+    input[i] = uint8(i + 1)
+
+  let bytes = vld1q_u8(input[0].addr)
+  vst1q_u8(output[0].addr, bytes)
+
+  doAssert output == input, "NEON load and store should preserve bytes."
+
+  echo "macOS NEON pointer test passed"
